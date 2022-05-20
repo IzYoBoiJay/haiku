d0852ebb035654d5f78b7888a5911577a5bbe34c
IzYoBoiJay <JTCalahatian@gmail.com>
2022-05-20 18:22:44 -0400

    Generate.js Changes & Styled Form + Textbox

--- src/components/Content/ContentElements.js
+++ src/components/Content/ContentElements.js
@@ -10,6 +10,8 @@ export const ContentContainer = styled.div`
 
 `;
 
+/* --------------------------------- Prompt --------------------------------- */
+
 export const PromptContainer = styled.div`
 
     top: 8vh;
@@ -21,13 +23,77 @@ export const PromptContainer = styled.div`
     background-color: #15202b;
     border-radius: 15px;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
     text-align: center;
     justify-content: center;
     font-size: ${FONT_LARGE};
     color: wheat;
 
 `;
+
+export const PromptForm = styled.form`
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  
+
+`;
+
+export const TextBox = styled.textarea`
+
+  width: 38vw;
+  height: 75vh;
+
+  display: flex;
+  text-align: left;
+  padding: 15px 10px 10px 10px;
+  border-radius: 1.1em;
+  margin-bottom: 5.5vh;
+
+`;
+
+export const SubmitButton = styled.input`
+
+  position: relative;
+  right: 10px;
+  bottom: 10px;
+  background: wheat;
+  white-space: nowrap;
+  padding: 16px 64px;
+  color: #fff;
+  font-size: 16px;
+  outline: none;
+  border: none;
+  border-radius: 30px;
+  cursor: pointer;
+  transition: 0.2s ease-in-out;
+  text-decoration: none;
+  text-align: center;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px; 
+  &:hover {
+    background: #fff;
+    transition: 0.2s ease-in-out;
+    cursor: pointer;
+    color: #000;
+  }
+  //breakpoints
+  @media screen and (max-width: 900px) {
+    padding: 12px 60px;
+  }
+  @media screen and (max-width: 750px) {
+    padding: 10px 30px;
+  }
+  @media screen and (max-width: 600px) {
+    padding: 8px 20px;
+  }
+`;
+
+/* -------------------------------- Response -------------------------------- */
 
 export const ResponseContainer = styled.div`
 
--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-import { ContentContainer, PromptContainer, ResponseContainer } from './ContentElements'
+import { ContentContainer, PromptContainer, PromptForm, ResponseContainer, SubmitButton, TextBox } from './ContentElements'
 
 const Content = () => {
   return (
     <ContentContainer>
 
-        <PromptContainer>Test</PromptContainer>
+        <PromptContainer>
+
+          <PromptForm>
+            <TextBox 
+            type="text"
+            id="text"
+            placeholder="What's happening?"
+            />
+            <SubmitButton type="submit"/>
+          </PromptForm>
+
+        </PromptContainer>
 
         <ResponseContainer>Test</ResponseContainer>
 
